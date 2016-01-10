763b2487fb33a2a67e9e29eef4a02a7e546daf5a
Andre Schloemp <andre.schloemp@me.com>
2016-01-10 13:00:33 +0100

    next So 10 Jan 2016 13:00:33 CET

--- Assets/_Scripts/ExitNextLevel.cs
+++ Assets/_Scripts/ExitNextLevel.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class ExitNextLevel : MonoBehaviour
 {
 	public Animator m_animator;
+	public GameObject m_activationObject;
 
 	// Use this for initialization
 	void Start ()
@@ -11,11 +12,20 @@ public class ExitNextLevel : MonoBehaviour
 		if (!m_animator) {
 			m_animator = GetComponent<Animator> ();
 		}
+		if (!m_activationObject) {
+			Transform lT = transform.FindChild ("Activate");
+			if (lT) {
+				m_activationObject = lT.gameObject;
+			}
+		}
 	}
 
 	void OnTriggerEnter (Collider aCollider)
 	{
 		if (aCollider.gameObject.tag == "Player") {
+			if (m_activationObject) {
+				m_activationObject.SetActive (true);
+			}
 			if (m_animator) {
 				m_animator.SetTrigger ("Activate");
 			}
