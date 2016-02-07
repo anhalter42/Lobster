0863a62ba34454fe25d6dedb2224fe576971e08b
Andre Schloemp <andre.schloemp@me.com>
2016-02-07 21:05:45 +0100

    next So  7 Feb 2016 21:05:45 CET

---
+++ Assets/AudioOnMove.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioOnMove : MonoBehaviour
+{
+
+	public float velocity = 0.2f;
+	public float angularVelocity = 0.2f;
+
+	Rigidbody m_Rigidbody;
+	AudioSource m_AudioSource;
+
+	// Use this for initialization
+	void Start ()
+	{
+		m_Rigidbody = GetComponent<Rigidbody> ();
+		m_AudioSource = GetComponent<AudioSource> ();
+	}
+
+	void FixedUpdate ()
+	{
+		if (m_Rigidbody == null || m_AudioSource == null)
+			return;
+		if (m_Rigidbody.angularVelocity.magnitude >= angularVelocity || m_Rigidbody.velocity.magnitude > velocity) {
+			if (!m_AudioSource.isPlaying)
+				m_AudioSource.Play ();
+		} else {
+			m_AudioSource.Stop ();
+		}
+	}
+}
